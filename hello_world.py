4c0c931f0c981af7f690a30ceeafefca71cea9f5
alex <lolmagexe@gmail.com>
2022-12-26 12:46:49 +0200

    (feat) added zero task

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+odds = []
+for i in range(60):
+	if (i % 2 == 1):
+		odds.append(i)
+	
+right_this_minute = datetime.today().minute
+
+if right_this_minute in odds:
+	print("This minute seems a little odd")
+else:
+	print("Not an odd minute")
+	
+print("This minute is {minute}".format(minute=right_this_minute))
